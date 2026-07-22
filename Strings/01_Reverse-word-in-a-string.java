73839e1af9b66e49e65f0b7eba4cfe2ebf313cc3
monikanamana0006-hub <monikanamana0006@gmail.com>
2026-07-22 20:11:27 +0530

    Update 01_Reverse-word-in-a-string.java

--- Strings/01_Reverse-word-in-a-string.java
+++ Strings/01_Reverse-word-in-a-string.java
@@ -35,3 +35,46 @@ class Solution {
         return String.join(" ", words);
     }
 }
+//optimal force
+class Solution {
+    // Function to reverse the order of words 
+    public String reverseWords(String s) {
+        // StringBuilder for final result
+        StringBuilder result = new StringBuilder();
+        
+        // Pointer starting from the end
+        int i = s.length() - 1;
+        
+        // Traverse from right to left
+        while (i >= 0) {
+            // Skip spaces
+            while (i >= 0 && s.charAt(i) == ' ') {
+                i--;
+            }
+            
+            // If pointer goes out of bounds, break
+            if (i < 0) break;
+            
+            // Mark end of word
+            int end = i;
+            
+            // Move left until space or start of string
+            while (i >= 0 && s.charAt(i) != ' ') {
+                i--;
+            }
+            
+            // Extract the word
+            String word = s.substring(i + 1, end + 1);
+            
+            // Add space before appending if result is not empty
+            if (result.length() > 0) {
+                result.append(" ");
+            }
+            
+            // Append word
+            result.append(word);
+        }
+        
+        return result.toString();
+    }
+}
